0e2a88b68e09ddd0e50acdab42f931f48c682cb7
giooo1 <giorgigakharia@gmail.com>
2024-02-01 18:19:51 +0400

    commit1

---
+++ src/components/Card-info.js
@@ -0,0 +1,28 @@
+import React from "react";
+import star from "../images/Star.png";
+
+export default function CardInfo(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  return (
+    <div className="card">
+      <img className="image" src={props.img} />
+      {badgeText && <div className="soldout">{badgeText}</div>}
+      <div className="star">
+        <img className="star-icon" src={star} />
+        <span className="gray">{props.rating}</span>
+        <span className="gray">({props.reviewCount}) *</span>
+        <span className="gray">{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
+}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,13 @@
 import React from "react";
+import CardInfo from "./Card-info";
 import photo from "../images/image.png";
-import star from "../images/Star.png";
+import photo2 from "../images/wedding-photography.png";
+import data from "../data";
 
 export default function Card() {
-  return (
-    <div className="card">
-      <img className="image" src={photo} />
-      <div className="soldout">SOLD OUT</div>
-      <div className="star">
-        <img className="star-icon" src={star} />
-        <span className="gray">5.0 *</span>
-        <span className="gray">(6)</span>
-      </div>
-      <p>Life lessons with Katie Zaferes</p>
-      <p>
-        <span className="bold">From $136</span> / person
-      </p>
-    </div>
-  );
+  const cards = data.map((item) => {
+    return <CardInfo key={item.id} img={photo} {...item} />;
+  });
+
+  return <div>{cards}</div>;
 }
